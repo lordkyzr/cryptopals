6b949658fbcc37e01ff7150dd0eff26058f7b593
Branden Jordan <branden.jordan@iovation.com>
2019-03-03 20:14:35 -0800

    just a little bit of cleanup on the first two challenges

--- set_1/challenge_2.py
+++ set_1/challenge_2.py
@@ -1,15 +1,10 @@
-from base64 import b64encode
 from codecs import decode, encode
 
-#Takes two hex strings and applys an xor
-def apply_xor(input_string,xor_string):
-    input_string_array = bytearray.fromhex(input_string)
-    xor_string_array = bytearray.fromhex(xor_string)
-    byte_array = bytearray(len(input_string))
 
-    for i in range(len(input_string_array)):
-        byte_array[i] = input_string_array[i] ^ xor_string_array[1]
-    return byte_array
+# Takes two hex strings and applys an xor
+def apply_xor(input_string,xor_string):
+    # bytes is iterable so zip down the two iterables and apply the xor
+    return bytes(x ^ y for x, y in zip(input_string, xor_string))
 
 
 def decode_string(input_string):
@@ -18,12 +13,14 @@ def decode_string(input_string):
 
 starting_string = "1c0111001f010100061a024b53535009181c"
 xor_string = "686974207468652062756c6c277320657965"
-expected_string = "746865206b696420646f6e277420706c6179"
+expected_string = b"746865206b696420646f6e277420706c6179"
 
 hex_string = decode_string(starting_string)
-print(type(hex_string))
 hex_xor_string = decode_string(xor_string)
-#xor_string = apply_xor(hex_string,hex_xor_string)
-xor_string = apply_xor(starting_string,xor_string)
+xor_string = apply_xor(hex_string,hex_xor_string)
+xor_string = encode(xor_string, "hex")
 
-print encode(bytes(xor_string),"hex")
+if xor_string == expected_string:
+    print("String Matched Expected String")
+else:
+    print("Strings did not match: Got {}, Expected: {}".format(xor_string,expected_string))
